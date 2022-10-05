73ae3d0b219649d1e6bfdb0b8c7ab8e43cd0b64f
hdurand22 <hdurand22@gmail.com>
2022-10-05 14:49:59 -0700

    added new images across the site

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef, useContext } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Breadcrumbs from './Breadcrumbs';
-import BoomBox from '../assets/boombox.jpg';
 import { ParallaxLayer } from '@react-spring/parallax';
 import { ScreenSizeContext } from '../App';
+import AboutIcon from './AboutIcon';
 
 const About = ({ parallax }) => {
   const [isShown, setIsShown] = useState(false);
@@ -38,7 +38,7 @@ const About = ({ parallax }) => {
             <ParallaxLayer className='layer' offset={2.2} speed={0.8} > 
               <Row xxl={2} xl={2} lg={2} md={1} sm={1} xs={1} className='section-content'>
                 <Col xxl={4} xl={6} md={5} sm={9} xs={9} className='section-image'>
-                  <img src={BoomBox} fluid='true' alt='Cassette Player Vectors by Vecteezy' />
+                  <AboutIcon />
                 </Col>
                 <Col xxl={6} xl={10} lg={11} md={11} sm={11} xs={11} className='section-text'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem viverra aliquet eget sit amet tellus. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Quam lacus suspendisse faucibus interdum posuere. Ut sem viverra aliquet eget. Quis varius quam quisque id diam. Fames ac turpis egestas integer eget aliquet. Nisi vitae suscipit tellus mauris a diam maecenas. Nisi scelerisque eu ultrices vitae auctor eu augue. Faucibus vitae aliquet nec ullamcorper sit amet risus. Tortor condimentum lacinia quis vel eros donec ac.
@@ -63,7 +63,7 @@ const About = ({ parallax }) => {
           <ParallaxLayer className='layer' offset={2.2} speed={0.8}>
             <Row xxl={2} xl={2} lg={2} md={1} sm={1} xs={1} className='section-content'>
                 <Col xxl={4} xl={6} md={5} sm={9} xs={9} className='section-image'>
-                  <img src={BoomBox} fluid='true' alt='Cassette Player Vectors by Vecteezy' />
+                  <AboutIcon />
                 </Col>
               <Col xxl={6} xl={10} lg={11} md={11} sm={11} xs={11} className='section-text'>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem viverra aliquet eget sit amet tellus. Posuere lorem ipsum dolor sit amet consectetur adipiscing. Quam lacus suspendisse faucibus interdum posuere. Ut sem viverra aliquet eget. Quis varius quam quisque id diam. Fames ac turpis egestas integer eget aliquet. Nisi vitae suscipit tellus mauris a diam maecenas. Nisi scelerisque eu ultrices vitae auctor eu augue. Faucibus vitae aliquet nec ullamcorper sit amet risus. Tortor condimentum lacinia quis vel eros donec ac.
